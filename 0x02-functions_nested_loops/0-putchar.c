08b4407864267cf5056276d86b72f5c1748ac727
Sarah <rahal.saraa@gmail.com>
2023-04-25 10:32:29 -0700

    chiblan

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * a program that prints _putchar, followed by a new line.
- * Return: On success 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
- 
-int main () {
-	printf ("_putchar\n");
-	return 0;
-}
 
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
 
