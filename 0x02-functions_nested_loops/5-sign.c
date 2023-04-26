d481f6fd4564752c655a621065b8d431219cb0eb
Sarah <rahal.saraa@gmail.com>
2023-04-26 07:27:52 -0700

    lambda

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - is the fuction that prints the sign of a num
+ *@n: is the value
+ *Return: 1 if +; -1 if -, 0 if its zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
