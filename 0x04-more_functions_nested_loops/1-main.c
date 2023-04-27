55d259c9bf83a48b11d0d2106634deb2544ba276
Sarah <rahal.saraa@gmail.com>
2023-04-27 16:13:47 -0700

    test

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,4 +1,3 @@
-1-main.c 
 #include "main.h"
 #include <stdio.h>
 
@@ -9,11 +8,11 @@
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    return (0);
+	c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isdigit(c));
+	return (0);
 }
