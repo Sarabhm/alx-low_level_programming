a84170a5c9e23f1b35fc95f1b9759c6d46d7e177
Sarah <rahal.saraa@gmail.com>
2023-05-03 15:19:56 -0700

    tes1

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - function printing half of a string
+ * @str: string to be printed
+ *return: always 0
+ */
+void puts_half(char *str)
+{
+	int len, j, i;
+
+	len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	j = len / 2;
+	if (len % 2)
+	{
+		j = (len - 1) / 2;
+	}
+	for (i = j; i < len; n++)
+	{
+		_putchar(*(str + i);
+	}
+	_putchar('\n');
+}
