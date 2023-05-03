82069ea4354ef85b32e3e9c705e6973866fb1af2
Sarah <rahal.saraa@gmail.com>
2023-05-03 14:26:40 -0700

    testt

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - prints every other char of a string
+ * @str: string that puts the chars from
+ * Retrun: Always 0
+ */
+void puts2(char *str)
+{
+	int lgth, a;
+
+	lgth = 0;
+
+	while (str[lgth] != '\0')
+	{
+		lgth++;
+	}
+
+	for (a = 0; a < len; a += 2)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}
