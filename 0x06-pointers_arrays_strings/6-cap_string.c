cbf7deff04810dad8e3f48706c501b4184383de5
Sarah <rahal.saraa@gmail.com>
2023-05-04 13:41:03 -0700

    captest

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to modify
+ * Return: the result of s
+ */
+char *cap_string(char *s)
+{
+	int i = 1;
+	int j = 0;
+	char separ[13] = " \t\n,;.!?\"(){}";
+
+	if (*s >= 'a' && *s <= 'z')
+	{
+		*s -= ('a' - 'A');
+	}
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j < 13 ; j++)
+		{
+			if (*(s + i) == s[j])
+			{
+				if (*(s + i + 1) >= 'a' && *(s + i + 1) <= 'z')
+				{
+					*(s + i + 1) -= ('a' - 'A');
+				}
+			}
+		}
+		i++;
+	}
+}
