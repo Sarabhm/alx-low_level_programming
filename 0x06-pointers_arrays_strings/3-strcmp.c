6a0acfa539cb15240b1f01a222a4ec38c69794b9
Sarah <rahal.saraa@gmail.com>
2023-05-04 09:58:13 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*_strcmp - compares two strings
+*@s1: first string to cmp
+*@s2: second string to cmp
+*Return: 0 if equals; if not return (s1 - s2)
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 = *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 != *s2)
+	{
+		return (*s1 - *s2);
+	}
+	else
+	{
+		return (0);
+	}
+}
