c5604ee91ab95847ce5cf9a3a742aaa86d49b43e
Sarah <rahal.saraa@gmail.com>
2023-05-04 11:49:05 -0700

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @s: string to modify
+ * Return: the result of s
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) -= ('a' - 'A');
+			i++;
+		}
+	}
+	return (s);
+}
+
