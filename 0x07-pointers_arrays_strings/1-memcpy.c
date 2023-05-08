7d7660994d9f4e28606208ab119cc6478330b88e
Sarah <rahal.saraa@gmail.com>
2023-05-08 06:15:59 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies a string
+ * @dest: copy
+ * @src: source
+ * @n: number of bytes
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (*(src + a) != '\0' && a < n)
+	{
+		a++;
+	}
+	for (b = 0; b < a; b++)
+	{
+		*(dest + b) = *(src + b);
+	}
+	return (dest);
+}
