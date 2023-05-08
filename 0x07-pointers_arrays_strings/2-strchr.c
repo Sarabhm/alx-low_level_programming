3e5bbc920ffd41df3ed26bed9ee626491ae3ae5a
Sarah <rahal.saraa@gmail.com>
2023-05-08 07:18:29 -0700

    tast

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
  *_strchr -  function that locates a character in a string.
  *@s: is the pointer
  *@c: is the value
- *Return: pointer s if c is found else return NULL
+ *Return: pointer s if c is found else return 0
  */
 char *_strchr(char *s, char c)
 {
