6fdcd9103fbef2556acad1ff7a587f0e613dc3bb
Sarah <rahal.saraa@gmail.com>
2023-05-12 11:47:28 -0700

    teste

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,40 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 
 	printf("%d\n", num1 * num2);
 	return (0);
+}
+/**
+ * _atoi - converts a string to an int
+ * @s: string to convert
+ * Return: int value of string
+ */
+int _atoi(char *s)
+{
+	int i = 1;
+	int res = 0;
+
+    if (*s == '-')
+    {
+        i = -1;
+        s++;
+    }
+
+    while (*s)
+    {
+        res = res * 10 + (*s - '0');
+        s++;
+    }
+
+    return (i * res);
 }
